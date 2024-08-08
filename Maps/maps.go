439cab54d01994af7f6b1a6f3e275eb554eeaaff
Beserker-356 <jenish.23bcs10016@ms.sst.scaler.com>
2024-08-08 23:14:24 +0530

    Added to Maps

--- Maps/maps.go
+++ Maps/maps.go
@@ -3,14 +3,24 @@ package main
 import "fmt"
 
 func main() {
+	// define a map
 	ages := make(map[string]int)
 	ages["John"] = 37
 	ages["Mary"] = 21
 
+	// define and initialize a map
 	ages2 := map[string]int{
 		"Jenish": 100,
 		"Jay": 50,
 	}
+
+	// delete a key
+	delete(ages, "Mary") 
+
+	// check if a key exists
+	ele, ok := ages2["Jenish"]
+
+	fmt.Println(ele, ok)
 
 	fmt.Println("Ages:", ages)
 	fmt.Println("Ages:", ages2)
